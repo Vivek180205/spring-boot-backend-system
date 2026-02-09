9a31214effcedeee490f4f569e566df9be547d4e
Vivek180205 <vivekrathod180205@gmail.com>
2026-02-09 18:34:41 +0530

    Removed sensitive credentials for push

---
+++ src/main/java/com/example/demo/security/JwtAuthFilter.java
@@ -0,0 +1,59 @@
+package com.example.demo.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.util.logging.Logger;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+
+            String token = header.substring(7);
+
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(jwtUtil.getKey())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            String username = claims.getSubject();
+            String role = claims.get("role", String.class);
+
+            var authToken =
+                    new UsernamePasswordAuthenticationToken(
+                            username,
+                            null,
+                            List.of(new SimpleGrantedAuthority("ROLE_" + role))
+                    );
+
+            SecurityContextHolder
+                    .getContext()
+                    .setAuthentication(authToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
